8d54c955292998c71b0af66bdedcd84c8b97c767
Draxler010 <daniel.rlg.99@gmail.com>
2022-08-29 20:39:24 -0700

    Nuevos cambios

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 // Constantes de Cache.
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
                
 // Almacenamos recursos estáticos al cache estático.
@@ -48,6 +48,10 @@ self.addEventListener('activate', e => {
         keys.forEach( key => {
 
             if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
